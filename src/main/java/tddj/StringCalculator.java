e3bc48b2660364b75a2ce542d8ecdeba45fc29f1
nicolas.betheuil <nicolas.betheuil@valtech.fr>
2016-12-07 16:13:08 +0100

    refactor

--- src/main/java/tddj/StringCalculator.java
+++ src/main/java/tddj/StringCalculator.java
@@ -5,9 +5,6 @@ import static java.lang.Integer.parseInt;
 public class StringCalculator {
     public int add(String s) {
         if ("".equals(s)) return 0;
-        if (!s.contains(",")) {
-            return parseInt(s);
-        }
         String[] values = s.split(",");
         int sum = 0;
         for (String value : values) {
