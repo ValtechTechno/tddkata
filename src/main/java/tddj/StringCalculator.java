c7ed11f564ad345d68887b9d71a6c31bc315b6e3
nicolas.betheuil <nicolas.betheuil@valtech.fr>
2016-12-07 16:22:29 +0100

    handle new line delimiter

--- src/main/java/tddj/StringCalculator.java
+++ src/main/java/tddj/StringCalculator.java
@@ -3,9 +3,12 @@ package tddj;
 import static java.lang.Integer.parseInt;
 
 public class StringCalculator {
+
+    private final String delimiters = "[,\n]";
+
     public int add(String s) {
         if ("".equals(s)) return 0;
-        String[] values = s.split(",");
+        String[] values = s.split(delimiters);
         int sum = 0;
         for (String value : values) {
             sum = sum + parseInt(value);
--- src/test/java/tddj/StringCalculatorTest.java
+++ src/test/java/tddj/StringCalculatorTest.java
@@ -31,6 +31,21 @@ public class StringCalculatorTest {
     }
 
     @Test
+    public void should_return_3_when_string_1_NEWLINE_2() {
+        assertThat(calculator.add("1\n2"), is(3));
+    }
+
+    @Test
+    public void should_return_the_sum_when_mixing_delimiters() {
+        assertThat(calculator.add("1\n2,3"), is(6));
+    }
+
+    @Test
+    public void should_return_the_number_value_when_there_is_one_empty() {
+        assertThat(calculator.add("1,\n"), is(1));
+    }
+
+    @Test
     public void should_return_the_3_when_string_1_comma_1_comma_1() {
         assertThat(calculator.add("1,1,1"), is(3));
     }
