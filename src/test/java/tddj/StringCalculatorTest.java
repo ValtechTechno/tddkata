df59169c17d20bacbe82a633333bd814d8bc3727
nicolas.betheuil <nicolas.betheuil@valtech.fr>
2016-12-08 19:59:55 +0100

    move to aspectj

--- src/test/java/tddj/StringCalculatorTest.java
+++ src/test/java/tddj/StringCalculatorTest.java
@@ -3,8 +3,8 @@ package tddj;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Fail.fail;
 
 public class StringCalculatorTest {
 
@@ -17,42 +17,42 @@ public class StringCalculatorTest {
 
     @Test
     public void should_return_0_when_empty_string() {
-        assertThat(calculator.add(""), is(0));
+        assertThat(calculator.add("")).isEqualTo(0);
     }
 
     @Test
     public void should_return_1_when_string_1() {
-        assertThat(calculator.add("1"), is(1));
+        assertThat(calculator.add("1")).isEqualTo(1);
     }
 
     @Test
     public void should_return_3_when_string_1_comma_2() {
-        assertThat(calculator.add("1,2"), is(3));
+        assertThat(calculator.add("1,2")).isEqualTo(3);
     }
 
     @Test
     public void should_return_3_when_string_1_NEWLINE_2() {
-        assertThat(calculator.add("1\n2"), is(3));
+        assertThat(calculator.add("1\n2")).isEqualTo(3);
     }
 
     @Test
     public void should_return_the_sum_when_mixing_delimiters() {
-        assertThat(calculator.add("1\n2,3"), is(6));
+        assertThat(calculator.add("1\n2,3")).isEqualTo(6);
     }
 
     @Test
     public void should_return_the_number_value_when_there_is_one_empty() {
-        assertThat(calculator.add("1,\n"), is(1));
+        assertThat(calculator.add("1,\n")).isEqualTo(1);
     }
 
     @Test
     public void should_return_the_3_when_string_1_comma_1_comma_1() {
-        assertThat(calculator.add("1,1,1"), is(3));
+        assertThat(calculator.add("1,1,1")).isEqualTo(3);
     }
 
     @Test
     public void should_use_the_delimiter_when_defined() {
-        assertThat(calculator.add("//;\n1;2"), is(3));
+        assertThat(calculator.add("//;\n1;2")).isEqualTo(3);
     }
 
     @Test(expected = NegativeNotAllowed.class)
@@ -66,7 +66,7 @@ public class StringCalculatorTest {
             calculator.add("-1,2,-1");
             fail("should not be here");
         } catch (NegativeNotAllowed negativeNotAllowed) {
-            assertThat(negativeNotAllowed.getMessage(), is("[-1, -1]"));
+            assertThat(negativeNotAllowed.getMessage()).isEqualTo("[-1, -1]");
         }
 
     }
